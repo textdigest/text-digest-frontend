bd0aa316b8ddcea6df451d3ca6ecc2d504b621ea
mmodrono <mannymodrono03@gmail.com>
2025-10-15 23:35:20 -0400

    Added example of useReaderSettings

--- src/app/(provider)/reader/page.tsx
+++ src/app/(provider)/reader/page.tsx
@@ -1,1 +1,25 @@
-export default function Page() {}
+'use client';
+import { useReaderSettings } from '@/hooks/reader/useReaderSettings';
+import { Button } from '@/components/ui/button';
+
+export default function ReaderDemo() {
+    const { font, fontClass, fontSize, toggleFont, increaseFont, decreaseFont } =
+        useReaderSettings();
+
+    return (
+        <div className='p-10'>
+            <Button className='text-white' onClick={toggleFont}>
+                Toggle Font ({font})
+            </Button>
+            <Button className='text-white' onClick={increaseFont}>
+                A+
+            </Button>
+            <Button className='text-white' onClick={decreaseFont}>
+                A-
+            </Button>
+            <p className={`${fontClass} transition-all`} style={{ fontSize: `${fontSize}px` }}>
+                TEST TEST TEST
+            </p>
+        </div>
+    );
+}
